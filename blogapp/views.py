262c91c323e88028034cad4880afe094a1d234e9
Jasur <tjasur224@gmail.com>
2023-12-21 16:53:47 +0500

    add madel, views, templates

--- blogapp/views.py
+++ blogapp/views.py
@@ -10,6 +10,10 @@ class PostListView(ListView):
     paginate_by = 2
     template_name = 'blog/post/list.html'
 
-def get_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, ublish__month=month, publish__day=day)
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug,
+                             status="published",
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
