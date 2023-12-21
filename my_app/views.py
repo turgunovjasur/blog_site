351ac88343d59784a76ca120c48fbd88ac8cf730
Jasur <tjasur224@gmail.com>
2023-12-21 16:17:17 +0500

    add madel, views, templates

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Post
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
+def get_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, ublish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
