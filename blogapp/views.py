840c4d58f623c4ebf593e26ed2139eee42cd733c
User <tjasur@gmail.com>
2023-12-25 14:10:12 +0500

    SETTINGS

--- blogapp/views.py
+++ blogapp/views.py
@@ -17,3 +17,4 @@ def post_detail(request, year, month, day, slug):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
